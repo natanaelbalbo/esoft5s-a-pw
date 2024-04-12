92c123110ba6ddce7487382568d193163309cde8
natanaelbalbo <145400090+natanaelbalbo@users.noreply.github.com>
2024-04-12 01:03:06 +0000

    exercicio11-04-24

--- pokemon.js
+++ pokemon.js
@@ -1,17 +1,82 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const evolutionName = urlParams.get('evolucao');
-    
-    fetch(`https://pokeapi.co/api/v2/pokemon/${evolutionName}`)
-      .then(response => response.json())
-      .then(data => {
-        const evolutionImg = document.createElement("img");
-        evolutionImg.src = data.sprites.front_default;
-        evolutionImg.alt = evolutionName;
-        evolutionImg.setAttribute("aria-label", `Imagem de ${evolutionName}`);
-        document.getElementById("evolution-img-container").appendChild(evolutionImg);
-        document.title = `Página do Pokémon ${evolutionName}`; // Correção aqui
-    })
-      .catch(error => console.error('Erro ao carregar imagem de evolução:', error));
+function changePageTitle(title) {
+  document.title = title;
+}
+
+function generateInfoSection(sprites, pokemonName) {
+  const h2 = document.createElement('h2');
+  h2.id = "info-pokemon-label";
+  h2.textContent = `Informações sobre ${pokemonName}`;
+
+  const img = document.createElement('img');
+  //const img = document.querySelector('img');
+  img.src = sprites[0];
+  img.alt = `Imagem do pokemon ${pokemonName}`;
+  let currentIndex = 0;
+
+  img.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % sprites.length;
+    img.src = sprites[currentIndex];
+  });
+
+  const section = document.querySelector('#info-evolucao');
+  section.appendChild(h2);
+  section.appendChild(img);
+}
+
+async function getPokemonData(name) {
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const jsonData = await data.json();
+
+    const spritesArray = Object.values(jsonData.sprites).filter(sprite => typeof sprite === 'string');
+    generateInfoSection(spritesArray, name);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getSearchParams() {
+
+  if (!location.search) {
+    return;
+  }
+
+  const urlSearchParams = new URLSearchParams(location.search);
+  
+
+  const pokemonName = urlSearchParams.get('evolucao');
+  
+  changePageTitle(`Pagina do ${pokemonName}`);
+  getPokemonData(pokemonName);
+}
+
+function updateCounter() {
+  const storageKey = 'pageVisitCount';
+  const now = new Date();
+  const formattedDate = new Intl.DateTimeFormat('pt-BR', {
+    year: 'numeric', month: '2-digit', day: '2-digit',
+    hour: '2-digit', minute: '2-digit'
+  }).format(now);
+
+  let visitData = localStorage.getItem(storageKey);
+  if (visitData) {
+    visitData = JSON.parse(visitData);
+    visitData.count++;
+    visitData.lastVisit = formattedDate;
+  } else {
+    visitData = { count: 1, lastVisit: formattedDate };
+  }
+
+  localStorage.setItem(storageKey, JSON.stringify(visitData));
+
+  const footer = document.querySelector('footer');
+  const visitInfoParagraph = document.createElement('p');
+  visitInfoParagraph.textContent = `Esta página foi visitada ${visitData.count} vezes. A última visita foi: ${visitData.lastVisit}`;
+
+
+  footer.appendChild(visitInfoParagraph);
+}
+document.addEventListener('DOMContentLoaded', function () {
+  getSearchParams();
+  updateCounter();
 });
